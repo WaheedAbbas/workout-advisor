17166afd4001a63c91aade62f8e35b6e772fabb8
Waheed Abbas <abbaxwaheed@gmail.com>
2024-01-15 12:55:53 +0500

    initial commit

---
+++ src/test/java/com/fitnessadvisor/workoutadvisor/WorkoutAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessadvisor.workoutadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
